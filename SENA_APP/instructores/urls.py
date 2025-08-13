d45f776c653447a151fabe561c1a070ca013267a
Alejandra <avelandia386@gmail.com>
2025-08-13 08:52:31 -0500

    Creamos el formulario de aprendices

--- SENA_APP/instructores/urls.py
+++ SENA_APP/instructores/urls.py
@@ -7,5 +7,5 @@ app_name = 'instructores'
 urlpatterns = [
     path('instructores/', views.instructores, name='lista_instructores'),
     path('instructores/instructor/<int:instructor_id>/', views.detalle_instructor, name='detalle_instructor'),
-    path('crear_instructor/', InstructorFormView.as_view(), name='crear_instructor')
+    path('/crear_instructor/', InstructorFormView.as_view(), name='crear_instructor')
 ]
